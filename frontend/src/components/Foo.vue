f2e47d6953d4a8ff364a5c8d2c47ef53001e1891
Sarah-Eswed <saraeswed@gmail.com>
2021-04-25 10:19:10 +0300

    changes

--- frontend/src/components/Foo.vue
+++ frontend/src/components/Foo.vue
@@ -1,56 +1,52 @@
 <template>
-<div class="homcontainer d-flex">
-<div class="left-sidedashboard bg-secondary">
- <Nav/>
-</div>
-<div class="content">
-   <router-view/>
-</div>
-</div>
+	<div class="homcontainer d-flex">
+		<div class="left-sidedashboard bg-secondary">
+			<Nav />
+		</div>
+		<div class="content">
+			<router-view />
+		</div>
+	</div>
 </template>
 
 <script>
-
-import {store} from "../store/store.js"
-import Nav from "../components/Left-side-Nav"
-
-
+import { store } from "../store/store.js";
+import Nav from "../components/Left-side-Nav";
 
 export default {
-  name: 'Foo',
+	name: "Foo",
 
-  data(){
-    return {
-      store:store,
-      data:""
-    }
-    },
-    mounted:function(){
-      !this.store.state.user.id ? this.$router.push('/login'):null;
-    },
-    components:{
-      Nav
-    }
-}
+	data() {
+		return {
+			store: store,
+			data: "",
+		};
+	},
+	mounted: function() {
+		!this.store.state.user.id ? this.$router.push("/login") : null;
+	},
+	components: {
+		Nav,
+	},
+};
 </script>
 
 <style scoped>
-.left-sidedashboard{
-  /* height: 100%; */
-  width: 15vw;
+.left-sidedashboard {
+	/* height: 100%; */
+	width: 15vw;
 }
-.incontent{
-   display: inline-flex;
-  flex-direction: column;
-  width: 100%;
+.incontent {
+	display: inline-flex;
+	flex-direction: column;
+	width: 100%;
 }
-.content{
-flex-grow: 1;
-   height: 100%;
-}
-
-.homcontainer{
-  flex-grow: 1;
+.content {
+	flex-grow: 1;
+	height: 100%;
 }
 
+.homcontainer {
+	flex-grow: 1;
+}
 </style>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,50 +1,55 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Login from '../components/Login'
-import Foo from '../components/Foo'
-import Trainees from "../pages/home/Index"
-import Dashboard from "../pages/home/Dashboard"
-import Home from "../pages/home/public/index"
-// import Navbar from "../components/Navbar"
+import Vue from "vue";
+import Router from "vue-router";
+import Login from "../components/Login";
+import Foo from "../components/Foo";
+import Trainees from "../pages/admin/Trainees";
+import Dashboard from "../pages/admin/Dashboard";
+import Home from "../pages/public/PublicIndex";
+import Technologies from "../pages/public/Technologies";
+import AboutUs from "../pages/public/AboutUs";
+import ContactUs from "../pages/public/ContactUs";
 
-
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
-  routes: [
-      {
-        path: '/admin*',
-        name: 'Admin',
-        component: Foo,
-        children: [
-          { path: '/admin/trainees',
-           component: Trainees ,
-          },
-          { path: '/admin/admins',
-           component: Trainees ,
-          },
-          { path: '/admin/contents',
-           component: Trainees ,
-          },
-          { path: '/admin/events',
-           component: Trainees ,
-          },
-          { path: '/',
-           component: Dashboard ,
-          },
-        
-        ]
-      },
-    {
-      path: '/login',
-      name: 'Login',
-      component: Login
-    },
-    {
-      path: '/',
-      name: 'Home',
-      component: Home
-    },
-    
-  ]
-})
+	mode: "history",
+	routes: [
+		{
+			path: "/admin*",
+			name: "Admin",
+			component: Foo,
+			children: [
+				{ path: "/admin/trainees", component: Trainees },
+				{ path: "/admin/admins", component: Trainees },
+				{ path: "/admin/contents", component: Trainees },
+				{ path: "/admin/events", component: Trainees },
+				{ path: "/", component: Dashboard },
+			],
+		},
+		{
+			path: "/login",
+			name: "Login",
+			component: Login,
+		},
+		{
+			path: "/",
+			name: "Home",
+			component: Home,
+		},
+		{
+			path: "/technologies",
+			name: "Technologies",
+			component: Technologies,
+		},
+		{
+			path: "/aboutUs",
+			name: "AboutUs",
+			component: AboutUs,
+		},
+		{
+			path: "/contactUs",
+			name: "ContactUs",
+			component: ContactUs,
+		},
+	],
+});
